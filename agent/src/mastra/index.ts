99005918f150e3af873056ee07fcddb0dfe9c510
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-04-09 22:08:42 +0900

    chore

--- agent/src/mastra/index.ts
+++ agent/src/mastra/index.ts
@@ -2,12 +2,18 @@
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 
-import { philosopherAgent } from './agents';
+import { philosopherAgent } from './agents/philosopherAgent';
+import {phenomenologistAgent} from "./agents/phenomenologistAgent"
+import {analyticPhilosopherAgent} from "./agents/analyticPhilosopherAgent"
 
 export const mastra = new Mastra({
-  agents: { philosopherAgent },
+  agents: { philosopherAgent ,phenomenologistAgent,analyticPhilosopherAgent},
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
   }),
 });
+
+
+
+
