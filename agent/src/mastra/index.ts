afc80f27365eb9f48deec81008795bbabb914566
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-04-09 22:02:51 +0900

    chore

--- agent/src/mastra/index.ts
+++ agent/src/mastra/index.ts
@@ -2,10 +2,10 @@
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 
-import { weatherAgent } from './agents';
+import { philosopherAgent } from './agents';
 
 export const mastra = new Mastra({
-  agents: { weatherAgent },
+  agents: { philosopherAgent },
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
